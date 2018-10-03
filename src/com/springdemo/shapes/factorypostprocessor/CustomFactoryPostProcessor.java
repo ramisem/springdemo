e468fd6050896fbb3366ef5dc3e342b43ca7f0c8
mandal.ramiseraja <rajaramise@gmail.com>
2018-10-03 17:53:03 +0530

    Test

---
+++ src/com/springdemo/shapes/factorypostprocessor/CustomFactoryPostProcessor.java
@@ -0,0 +1,14 @@
+package com.springdemo.shapes.factorypostprocessor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+public class CustomFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        System.out.println("Calling postProcessBeanFactory: ");
+
+    }
+}
