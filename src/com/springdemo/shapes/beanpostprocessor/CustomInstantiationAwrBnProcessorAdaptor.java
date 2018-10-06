17e22e745a76596d3e3691123bd1f7e2de03e89e
mandal.ramiseraja <rajaramise@gmail.com>
2018-10-06 10:17:42 +0530

    Test

---
+++ src/com/springdemo/shapes/beanpostprocessor/CustomInstantiationAwrBnProcessorAdaptor.java
@@ -0,0 +1,48 @@
+package com.springdemo.shapes.beanpostprocessor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.PropertyValues;
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
+
+import java.beans.PropertyDescriptor;
+
+public class CustomInstantiationAwrBnProcessorAdaptor implements InstantiationAwareBeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException {
+        System.out.println("1");
+//        System.out.println("CustomInstantiationAwrBnProcessorAdaptor.postProcessBeforeInstantiation");
+        return beanClass;
+    }
+
+    @Override
+    public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
+        System.out.println("2");
+        if(bean!=null) {
+//            System.out.println("CustomInstantiationAwrBnProcessorAdaptor.postProcessAfterInstantiation returns true");
+            return true;
+        }
+        System.out.println("CustomInstantiationAwrBnProcessorAdaptor.postProcessAfterInstantiation returns false");
+        return false;
+    }
+
+    @Override
+    public PropertyValues postProcessPropertyValues(PropertyValues pvs, PropertyDescriptor[] pds, Object bean, String beanName) throws BeansException {
+        System.out.println("3");
+//        System.out.println("CustomInstantiationAwrBnProcessorAdaptor.postProcessPropertyValues");
+        return pvs;
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("4");
+//        System.out.println("CustomInstantiationAwrBnProcessorAdaptor.postProcessBeforeInitialization");
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("5");
+        return bean;
+    }
+}
